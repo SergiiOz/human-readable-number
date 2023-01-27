c17362c88feef5d88568362697b1574de31ad17e
unknown <s.ozinkovskyi@gmail.com>
2023-01-27 09:23:23 +0200

    feat: add func oneToTen

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function toReadable (number) {
-  
+  const oneToTen = (num) = > {
+    switch (num) {
+        case 1:
+            return "one"
+        case 2:
+            return "two"
+        case 3:
+            return "three"
+        case 4:
+            return "four"
+        case 5:
+            return "five"
+        case 6:
+            return "six"
+        case 7:
+            return "seven"
+        case 8:
+            return "eight"
+        case 9:
+            return "eight"
+        case 10:
+            return "nine"    
+        default:
+            return '';
+    }
+  }
+
+    
 }
