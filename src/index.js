315fbde31150e62ab6f4add70ebde2a8ca0e02b5
unknown <s.ozinkovskyi@gmail.com>
2023-01-27 09:37:39 +0200

    fix: syntaxerror brackets

--- src/index.js
+++ src/index.js
@@ -1,81 +1,76 @@
-module.exports = function toReadable (number) {
-  const oneToTen = (num) = > {
-    switch (num) {
-        case 1:
-            return "one"
-        case 2:
-            return "two"
-        case 3:
-            return "three"
-        case 4:
-            return "four"
-        case 5:
-            return "five"
-        case 6:
-            return "six"
-        case 7:
-            return "seven"
-        case 8:
-            return "eight"
-        case 9:
-            return "eight"
-        case 10:
-            return "nine"    
-        default:
-            return '';
-    }
-  }
+module.exports = function toReadable(number) {
+    const oneToTen = (num) => {
+        switch (num) {
+            case 1:
+                return "one";
+            case 2:
+                return "two";
+            case 3:
+                return "three";
+            case 4:
+                return "four";
+            case 5:
+                return "five";
+            case 6:
+                return "six";
+            case 7:
+                return "seven";
+            case 8:
+                return "eight";
+            case 9:
+                return "eight";
+            case 10:
+                return "nine";
+            default:
+                return "";
+        }
+    };
 
-  const elevenToNineteen = (num) = > {
-    switch (num) {
-        case 1:
-            return "eleven"
-        case 2:
-            return "twelve"
-        case 3:
-            return "thirteen"
-        case 4:
-            return "fourteen"
-        case 5:
-            return "fifteen"
-        case 6:
-            return "sixteen"
-        case 7:
-            return "seventeen"
-        case 8:
-            return "eighteen"
-        case 9:
-            return "nineteen"
-         default:
-            return '';
-    }
-  }
+    const elevenToNineteen = (num) => {
+        switch (num) {
+            case 1:
+                return "eleven";
+            case 2:
+                return "twelve";
+            case 3:
+                return "thirteen";
+            case 4:
+                return "fourteen";
+            case 5:
+                return "fifteen";
+            case 6:
+                return "sixteen";
+            case 7:
+                return "seventeen";
+            case 8:
+                return "eighteen";
+            case 9:
+                return "nineteen";
+            default:
+                return "";
+        }
+    };
 
-  const dozens = (num) = > {
-    switch (num) {
-        case 2:
-            return "twenty"
-        case 3:
-            return "thirty"
-        case 4:
-            return "fourty"
-        case 5:
-            return "fifty"
-        case 6:
-            return "sixty"
-        case 7:
-            return "seventy"
-        case 8:
-            return "eighty"
-        case 9:
-            return "ninety"
-         default:
-            return '';
-    }
-  }
-
-
-
-  
-    
-}
+    const dozens = (num) => {
+        switch (num) {
+            case 2:
+                return "twenty";
+            case 3:
+                return "thirty";
+            case 4:
+                return "fourty";
+            case 5:
+                return "fifty";
+            case 6:
+                return "sixty";
+            case 7:
+                return "seventy";
+            case 8:
+                return "eighty";
+            case 9:
+                return "ninety";
+            default:
+                return "";
+        }
+    };
+};
